fd21d7f4673774110908a3f0b2769c8386b4b5ae
Ben <bostrower3@gatech.edu>
2025-05-23 14:39:15 -0400

    built

--- Poonam_Docker_Submission/download.py
+++ Poonam_Docker_Submission/download.py
@@ -1,4 +1,8 @@
 from huggingface_hub import snapshot_download
-snapshot_download("gpt2", local_dir="./gpt2", local_dir_use_symlinks=False)
-snapshot_download("bert-base-uncased", local_dir="./bert-base-uncased", local_dir_use_symlinks=False)
-do
+#snapshot_download("gpt2", local_dir="./gpt2", local_dir_use_symlinks=False)
+#snapshot_download("bert-base-uncased", local_dir="./bert-base-uncased", local_dir_use_symlinks=False)
+
+
+import nltk
+nltk.download("punkt", download_dir="./nltk_data")
+nltk.download("punkt_tab", download_dir="./nltk_data")
